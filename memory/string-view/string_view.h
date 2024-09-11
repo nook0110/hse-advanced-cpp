c6f6e0cdaa8fc098eb331d49ddb73cb61d49117f
Nikita Blokhtin <nik.blokhtin@gmail.com>
2024-09-11 18:42:35 +0300

    memory/string-view

--- memory/string-view/string_view.h
+++ memory/string-view/string_view.h
@@ -3,13 +3,14 @@
 #include <algorithm>
 #include <cstddef>
 #include <string>
+#include <cstring>
 
 class StringView {
 public:
     StringView(const std::string& string, size_t start = 0, size_t len = std::string::npos)
         : data_(string.data() + start), size_(std::min(string.size() - start, len)) {
     }
-    StringView(const char* c_string) : data_(c_string), size_(strlen(c_string)) {
+    StringView(const char* c_string) : data_(c_string), size_(std::strlen(c_string)) {
     }
     StringView(const char* data, size_t size) : data_(data), size_(size) {
     }
