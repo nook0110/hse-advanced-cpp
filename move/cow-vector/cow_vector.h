00f339f9a211ea176585ce98cce7561ea6ca5aad
Nikita Blokhtin <nik.blokhtin@gmail.com>
2024-09-15 19:52:12 +0300

    move/cow-vector

--- move/cow-vector/cow_vector.h
+++ move/cow-vector/cow_vector.h
@@ -7,18 +7,9 @@
 #include <vector>
 
 struct State {
-    void Subscibe() {
-        ++ref_count;
-    }
-    void Unsubscibe() {
-        --ref_count;
-        if (ref_count == 0) {
-            delete this;
-        }
-    }
-    std::unique_ptr<State> GetCopy() const {
-        return std::make_unique<State>(1, data);
-    }
+    void Subscibe();
+    void Unsubscibe();
+    std::unique_ptr<State> GetCopy() const;
 
     size_t ref_count = 1;
     std::vector<std::string> data;
@@ -26,70 +17,30 @@ struct State {
 
 class COWVector {
 public:
-    COWVector() : state_(new State) {
-    }
-    ~COWVector() {
-        state_->Unsubscibe();
-    }
+    COWVector();
+    ~COWVector();
 
-    COWVector(const COWVector& other) : state_(other.state_) {
-        state_->Subscibe();
-    }
-    COWVector& operator=(const COWVector& other) {
-        state_->Unsubscibe();
-        state_ = other.state_;
-        state_->Subscibe();
-        return *this;
-    }
+    COWVector(const COWVector& other);
+    COWVector& operator=(const COWVector& other);
 
-    COWVector(COWVector&& other) {
-        Swap(other);
-    }
+    COWVector(COWVector&& other);
 
-    COWVector& operator=(COWVector&& other) {
-        Swap(other);
-        return *this;
-    }
+    COWVector& operator=(COWVector&& other);
 
-    size_t Size() const {
-        return state_->data.size();
-    }
+    size_t Size() const;
 
-    void Resize(size_t size) {
-        GoingToChange();
+    void Resize(size_t size);
+    const std::string& Get(size_t at) const;
+    const std::string& Back() const;
 
-        state_->data.resize(size);
-    }
+    void PushBack(const std::string& value);
 
-    const std::string& Get(size_t at) const {
-        return state_->data[at];
-    }
-    const std::string& Back() const {
-        return state_->data.back();
-    }
+    void Set(size_t at, const std::string& value);
 
-    void PushBack(const std::string& value) {
-        GoingToChange();
-        state_->data.push_back(value);
-    }
-
-    void Set(size_t at, const std::string& value) {
-        GoingToChange();
-        state_->data[at] = value;
-    }
-
-    void Swap(COWVector& other) {
-        std::swap(state_, other.state_);
-    }
+    void Swap(COWVector& other);
 
 private:
-    void GoingToChange() {
-        if (state_->ref_count > 1) {
-            auto new_state = state_->GetCopy();
-            state_->Unsubscibe();
-            state_ = new_state.release();
-        }
-    }
+    void GoingToChange();
 
     State* state_ = nullptr;
 };
