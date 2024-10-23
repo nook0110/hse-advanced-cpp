894c45c09b7fde46b8d33401856a4a0628143498
Nikita Blokhtin <nik.blokhtin@gmail.com>
2024-10-23 21:33:36 +0300

    meta/compile-eval

--- meta/compile-eval/sqrt.h
+++ meta/compile-eval/sqrt.h
@@ -1,16 +1,22 @@
 
 #include <cmath>
-template <bool B, class T, int ANS>
+template <bool B, class T, class F>
 struct Conditional {
     static const int value = T::value;  // NOLINT
 };
 
-template <class T, int ANS>
-struct Conditional<false, T, ANS> {
-    static const int value = ANS;  // NOLINT
+template <class T, class F>
+struct Conditional<false, T, F> {
+    static const int value = F::value;  // NOLINT
 };
 
-template <int N, int ANS = static_cast<int>(sqrt(N) + 1)>
+template <int N, int L = 0, int R = N + 1, int M = (L + R) / 2>
 struct Sqrt {
-    static const int value = Conditional<(ANS * ANS >= N), Sqrt<N, ANS - 1>, ANS + 1>::value;  // NOLINT
+    static const int value =  // NOLINT
+        Conditional<(M >= (N + M - 1) / M), Sqrt<N, L, M>, Sqrt<N, M, R> >::value;
+};
+
+template <int N, int ANS>
+struct Sqrt<N, ANS, ANS + 1> {
+    static const int value = ANS + 1;  // NOLINT
 };
