b4fb2978ef4ddc3dcc9c91ac5e048a938773f487
Nikita Blokhtin <nik.blokhtin@gmail.com>
2024-11-27 19:36:43 +0300

    condvars/buffered-channel

---
+++ condvars/buffered-channel/buffered_channel.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <condition_variable>
+#include <cstddef>
+#include <mutex>
+#include <queue>
+#include <stdexcept>
+#include <utility>
+#include <optional>
+
+template <class T>
+class BufferedChannel {
+public:
+    explicit BufferedChannel(int size) : size_(size) {
+        // Your code goes here
+    }
+
+    void Send(const T& value) {
+        std::unique_lock lock(mutex_);
+
+        cv_.wait(lock, [this]() { return data_.size() < size_ || closed_; });
+
+        if (closed_) {
+            cv_.notify_all();
+            throw std::runtime_error("wat r u doing bro");
+        }
+
+        data_.emplace(value);
+        cv_.notify_all();
+    }
+
+    std::optional<T> Recv() {
+        std::unique_lock lock(mutex_);
+
+        cv_.wait(lock, [this]() { return !data_.empty() || closed_; });
+
+        if (closed_ && data_.empty()) {
+            cv_.notify_all();
+            return {};
+        }
+
+        auto ans = data_.front();
+        data_.pop();
+        cv_.notify_all();
+        return ans;
+    }
+
+    void Close() {
+        std::unique_lock lock(mutex_);
+        closed_ = true;
+        cv_.notify_all();
+    }
+
+    bool closed_ = false;
+
+    std::queue<T> data_;
+    std::size_t size_;
+
+    std::mutex mutex_;
+    std::condition_variable cv_;
+};
