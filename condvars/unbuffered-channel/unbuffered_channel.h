bef88dcc36995158be57bcb7ad62b2b96f14a849
Nikita Blokhtin <nik.blokhtin@gmail.com>
2024-11-27 21:03:06 +0300

    condvars/unbuffered-channel

--- condvars/unbuffered-channel/unbuffered_channel.h
+++ condvars/unbuffered-channel/unbuffered_channel.h
@@ -16,53 +16,62 @@ public:
     void Send(const T& value) {
         std::unique_lock lock(mutex_);
 
-        cv_.wait(lock, [this]() { return data_.size() < size_ || closed_; });
+        waiting_sender_.wait(lock, [this]() { return !sending_ || closed_; });
+
+        sending_ = true;
 
         if (closed_) {
-            cv_.notify_all();
             throw std::runtime_error("wat r u doing bro");
         }
 
         data_.emplace(value);
-        read = false;
-        cv_.notify_all();
-        cv_.wait(lock, [this]() { return read || closed_; });
-        if (closed_ && !read) {
-            cv_.notify_all();
+        reader_.notify_one();
+        sended_sender_.wait(lock, [this]() { return !data_ || closed_; });
+
+        if (closed_ && data_) {
             throw std::runtime_error("wat r u doing bro");
         }
-        cv_.notify_all();
+
+        sending_ = false;
+        waiting_sender_.notify_one();
     }
 
     std::optional<T> Recv() {
         std::unique_lock lock(mutex_);
 
-        cv_.wait(lock, [this]() { return !data_.empty() || closed_; });
+        reader_.wait(lock, [this]() { return data_ || closed_; });
 
-        if (closed_ && data_.empty()) {
-            cv_.notify_all();
+        if (closed_) {
             return {};
         }
 
-        auto ans = data_.front();
-        data_.pop();
-        read = true;
-        cv_.notify_all();
+        auto ans = *data_;
+        data_ = {};
+        sended_sender_.notify_one();
         return ans;
     }
 
     void Close() {
         std::unique_lock lock(mutex_);
         closed_ = true;
-        cv_.notify_all();
+        NotifyAll();
     }
 
     bool closed_ = false;
 
-    std::queue<T> data_;
+    std::optional<T> data_;
     std::size_t size_;
 
     std::mutex mutex_;
-    std::condition_variable cv_;
-    bool read = false;
+    std::condition_variable waiting_sender_;
+    std::condition_variable sended_sender_;
+    std::condition_variable reader_;
+
+    bool sending_ = false;
+
+    void NotifyAll() {
+        waiting_sender_.notify_all();
+        sended_sender_.notify_all();
+        reader_.notify_all();
+    }
 };
